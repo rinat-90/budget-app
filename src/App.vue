c1cf11c60adb07d2ef69a7b414495fc5bd5a5d13
rinat-90 <ribroff@gmail.com>
2020-02-12 13:46:20 -0600

    completed layout and page implimentation

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <component :is="layout">
+      <router-view />
+    </component>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import EmptyLayout from './layouts/EmptyLayout.vue';
+  import MainLayout from './layouts/MainLayout.vue';
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  export default {
+    name: 'App',
+    components: { EmptyLayout, MainLayout  },
+    computed: {
+      layout() {
+        return (this.$route.meta.layout || 'empty') + '-layout'
+      }
     }
   }
-}
+</script>
+
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.css";
+@import "assets/index.css";
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -8,15 +7,55 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: () => import('../views/Home.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/login',
+    name: 'Login',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Login.vue'),
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    meta: { layout: 'empty' },
+    component: () => import('../views/Register.vue'),
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    meta: { layout: 'main' },
+    component: () => import('../views/Profile.vue'),
+  },
+  {
+    path: '/record',
+    name: 'Record',
+    meta: { layout: 'main' },
+    component: () => import('../views/Record.vue'),
+  },
+  {
+    path: '/record-details',
+    name: 'RecordDetails',
+    meta: { layout: 'main' },
+    component: () => import('../views/RecordDetails.vue'),
+  },
+  {
+    path: '/planing',
+    name: 'Planing',
+    meta: { layout: 'main' },
+    component: () => import('../views/Planing.vue'),
+  },
+  {
+    path: '/history',
+    name: 'History',
+    meta: { layout: 'main' },
+    component: () => import('../views/History.vue'),
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    meta: { layout: 'main' },
+    component: () => import('../views/Categories.vue'),
   },
 ];
 
